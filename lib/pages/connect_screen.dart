15bc45c451707209da0885396555122be89f8c44
Mukund Solanki <mukundsolanki665@gmail.com>
2023-12-24 18:38:17 +0530

    Added animation and navigator to perform video call

--- lib/pages/connect_screen.dart
+++ lib/pages/connect_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+// import 'widgets/call.dart';
 
 class ConnectScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Connect Screen'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              'assets/lottie/call2.json',
+              width: 400,
+            ),
+            // Start Call Button
+            ElevatedButton.icon(
+              onPressed: () {
+                // Call ID is used to establish connection between two diifferent users
+                // Navigator.push(context, MaterialPageRoute(builder: (context) => const CallPage(callID: 'User1connectsUser2'),),);
+              },
+              icon: Icon(Icons.video_call),
+              label: Text('Start Call'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
