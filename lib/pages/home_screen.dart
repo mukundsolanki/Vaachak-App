769e801a63042c07c2e7829d6c26c826fc6c475c
Mukund Solanki <mukundsolanki665@gmail.com>
2023-12-24 13:27:51 +0530

    Sync Icon rotation functionality added

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 import 'widgets/bottom_model_sheet.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -6,12 +7,24 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  List<String> texts = [];
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  List<TextInfo> texts = [];
+  late AnimationController _rotateController;
 
   void addText(String newText) {
     setState(() {
-      texts.add(newText);
+      texts.add(TextInfo(text: newText, isLoading: true));
+    });
+
+    // Simulate loading for 5 seconds
+    Future.delayed(Duration(seconds: 5), () {
+      setState(() {
+        // Update the loading state to false after 5 seconds
+        texts.last.isLoading = false;
+      });
+
+      _rotateController.stop();
     });
   }
 
@@ -19,6 +32,23 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       texts.removeAt(index);
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _rotateController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    );
+
+    _rotateController.repeat();
+  }
+
+  @override
+  void dispose() {
+    _rotateController.dispose();
+    super.dispose();
   }
 
   @override
@@ -31,7 +61,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 itemCount: texts.length,
                 itemBuilder: (context, index) {
                   return Container(
-                    margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), // Adjust spacing
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                     decoration: BoxDecoration(
                       color: Colors.grey[200],
                       borderRadius: BorderRadius.circular(12.0),
@@ -41,22 +72,32 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Expanded(
                           child: Text(
-                            texts[index],
+                            texts[index].text,
                             style: TextStyle(
                               fontSize: 22.0,
                             ),
                           ),
                         ),
                         SizedBox(width: 16.0),
-                        IconButton(
-                          icon: Icon(
-                            Icons.check_circle,
-                            color: Colors.green,
-                          ),
-                          onPressed: () {
-                          },
-                        ),
-                        // SizedBox(width: 2.0),
+                        texts[index].isLoading
+                            ? AnimatedBuilder(
+                                animation: _rotateController,
+                                builder: (context, child) {
+                                  return Transform.rotate(
+                                    angle:
+                                        _rotateController.value * 5.0 * math.pi,
+                                    child: Icon(
+                                      Icons.sync_rounded,
+                                      color: Colors.blue,
+                                    ),
+                                  );
+                                },
+                              )
+                            : Icon(
+                                Icons.check_circle,
+                                color: Colors.green,
+                              ),
+                        SizedBox(width: 8.0),
                         IconButton(
                           icon: Icon(
                             Icons.delete,
@@ -77,7 +118,8 @@ class _HomeScreenState extends State<HomeScreen> {
           showModalBottomSheet(
             isScrollControlled: true,
             context: context,
-            builder: (BuildContext context) => MyBottomSheet(onTextCreated: addText),
+            builder: (BuildContext context) =>
+                MyBottomSheet(onTextCreated: addText),
           );
         },
         label: Text('ADD SIGN'),
@@ -85,4 +127,11 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
+
+class TextInfo {
+  final String text;
+  bool isLoading;
+
+  TextInfo({required this.text, this.isLoading = false});
 }
