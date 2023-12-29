cf7b63fcc0a8cf553d32f0a9ead7a79270adf141
Gresey Patidar <113979215+Gresey@users.noreply.github.com>
2023-12-29 18:17:08 +0530

    Update home_screen.dart

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 import 'widgets/bottom_model_sheet.dart';
-
+import 'package:video_player/video_player.dart';
 class HomeScreen extends StatefulWidget {
-  String get title => 'as';
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -13,6 +11,37 @@ class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   List<TextInfo> texts = [];
   late AnimationController _rotateController;
+  late VideoPlayerController _videoController;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _rotateController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    );
+    _rotateController.repeat();
+
+    _initializeVideoController();
+   // _initializeVideoPlayerFuture=_videoController.initialize();// Initialize video controller here
+  }
+
+  Future<void> _initializeVideoController() async {
+    final String videoUrl =
+        'https://youtu.be/Gq6OKFugRqY?si=yiOp3Rl0c4jROSti';
+    _videoController = VideoPlayerController.networkUrl(videoUrl as Uri);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _rotateController.dispose();
+    _videoController.dispose();
+    super.dispose();
+  }
 
   void addText(String newText) {
     setState(() {
@@ -22,10 +51,8 @@ class _HomeScreenState extends State<HomeScreen>
     // Simulate loading for 5 seconds
     Future.delayed(Duration(seconds: 5), () {
       setState(() {
-        // Update the loading state to false after 5 seconds
         texts.last.isLoading = false;
       });
-
       _rotateController.stop();
     });
   }
@@ -34,23 +61,6 @@ class _HomeScreenState extends State<HomeScreen>
     setState(() {
       texts.removeAt(index);
     });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _rotateController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 5),
-    );
-
-    _rotateController.repeat();
-  }
-
-  @override
-  void dispose() {
-    _rotateController.dispose();
-    super.dispose();
   }
 
   @override
@@ -66,84 +76,108 @@ class _HomeScreenState extends State<HomeScreen>
             child: Text(
               'Hello User,',
               style: TextStyle(
-                  fontSize: 30.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                  fontStyle: FontStyle.italic),
+                fontSize: 30.0,
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+              ),
             ),
           ),
           Expanded(
             child: Center(
               child: texts.isEmpty
                   ? Text(
-                      'No Signs Yet',
-                      style: TextStyle(
-                        color: Colors.black87,
-                      ),
-                    )
+                'No Signs Yet',
+                style: TextStyle(
+                  color: Colors.black87,
+                ),
+              )
                   : ListView.builder(
-                      itemCount: texts.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 70,
-                          margin: EdgeInsets.symmetric(
-                              horizontal: 16.0, vertical: 8.0),
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade300,
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          padding: EdgeInsets.all(16.0),
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  texts[index].text,
-                                  style: TextStyle(
-                                    fontSize: 22.0,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 16.0),
-                              texts[index].isLoading
-                                  ? AnimatedBuilder(
-                                      animation: _rotateController,
-                                      builder: (context, child) {
-                                        return Transform.rotate(
-                                          angle: _rotateController.value *
-                                              5.0 *
-                                              math.pi,
-                                          child: Icon(
-                                            Icons.sync_rounded,
-                                            color: Colors.blue,
-                                          ),
-                                        );
-                                      },
-                                    )
-                                  : Icon(
-                                      Icons.check_circle,
-                                      color: Colors.green,
-                                    ),
-                              SizedBox(width: 8.0),
-                              IconButton(
-                                icon: Icon(
-                                  Icons.delete,
-                                  color: Colors.red,
-                                ),
-                                onPressed: () {
-                                  removeText(index);
-                                },
-                              ),
-                            ],
-                          ),
-                        );
-                      },
+                itemCount: texts.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 200,
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 19.0,
+                      vertical: 10.0,
                     ),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                texts[index].text,
+                                style: TextStyle(
+                                  fontSize: 22.0,
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16.0),
+                            texts[index].isLoading
+                                ? AnimatedBuilder(
+                              animation: _rotateController,
+                              builder: (context, child) {
+                                return Transform.rotate(
+                                  angle: _rotateController.value *
+                                      5.0 *
+                                      3.14,
+                                  child: Icon(
+                                    Icons.sync_rounded,
+                                    color: Colors.blue,
+                                  ),
+                                );
+                              },
+                            )
+                                : Icon(
+                              Icons.check_circle,
+                              color: Colors.green,
+                            ),
+                            SizedBox(width: 8.0),
+                            IconButton(
+                              icon: Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                              onPressed: () {
+                                removeText(index);
+                              },
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 3),
+                        FutureBuilder(future: _initializeVideoPlayerFuture,
+                            builder: (context,snapshot){
+                          if(snapshot.connectionState==ConnectionState.done){
+                            return Center(
+                            child:AspectRatio(
+                            aspectRatio: _videoController.value.aspectRatio,
+                  child: VideoPlayer(_videoController),
+                  ),);}else{
+                            return Center(
+                      child: CircularProgressIndicator(),
+                            );
+                  }
+                  },
+                        ),
+
+                      //  _videoController.value.isInitialized
+
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        // backgroundColor: Colors.blueGrey.shade300,
         backgroundColor: Color.fromARGB(255, 207, 61, 51),
         onPressed: () {
           showModalBottomSheet(
@@ -152,6 +186,7 @@ class _HomeScreenState extends State<HomeScreen>
             builder: (BuildContext context) =>
                 MyBottomSheet(onTextCreated: addText),
           );
+          // To be implemented: Show modal bottom sheet or add text functionality
         },
         label: Text(
           'ADD SIGN',
@@ -175,3 +210,4 @@ class TextInfo {
 
   TextInfo({required this.text, this.isLoading = false});
 }
+
