5cb80cd108e31241e2098ba2eb11246b66c8c180
Mukund Solanki <mukundsolanki665@gmail.com>
2023-12-31 11:30:30 +0530

    Updated the code for login screen TODO: Add bg video

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -1,96 +1,44 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _ipAddressController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('images/device_image1.jpg'), fit: BoxFit.cover)),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Colors.transparent,
-                  Colors.transparent,
-                  Color(0xff161d27).withOpacity(0.9),
-                  Color(0xff161d27),
-                ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Text(
-                  "Welcome,",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 45,
-                      //fontWeight: FontWeight.bold
-                  ),
-                ),
-                SizedBox(
-                  height: 13,
-                ),
-                 Text(
-                   "Enter Your Device IP Address to Pair",
-                   style: TextStyle(color: Colors.grey.shade500, fontSize: 16),
-                 ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  height: 50,
-                  margin: EdgeInsets.only(left: 25, right: 25),
-                  child: TextField(
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                    decoration: InputDecoration(
-                      hintText: "  IP Address",
-                      hintStyle: TextStyle(color: Colors.grey.shade700),
-                      filled: true,
-                      fillColor: Color(0xff161d27).withOpacity(0.9),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide(color: Color(0xffffffff))),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide(color: Color(0xffffffff))),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-
-
-                Container(
-                  height: 35,
-                  width: 100,
-                  margin: EdgeInsets.only(left: 50, right: 50),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "SIGN IN",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 13),
-                    ),
-                    style:ElevatedButton.styleFrom(backgroundColor: Colors.white,
-                    side: BorderSide(width: 0.3,color:Color(0xffffffff)),)
-                  ),
-                ),
-                SizedBox(height: 70,)
-
-
-              ],
+      appBar: AppBar(
+        title: Text('Login Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _ipAddressController,
+              keyboardType: TextInputType.numberWithOptions(
+                decimal: true,
+                signed: false,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Enter IP Address',
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                String ipAddress = _ipAddressController.text;
+                print('Entered IP Address: $ipAddress');
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
   }
