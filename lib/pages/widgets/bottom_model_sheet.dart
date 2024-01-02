332b904ad85547d03be90a0d6989ab7e724b7812
Gresey Patidar <113979215+Gresey@users.noreply.github.com>
2024-01-02 17:32:44 +0530

    Update bottom_model_sheet.dart

--- lib/pages/widgets/bottom_model_sheet.dart
+++ lib/pages/widgets/bottom_model_sheet.dart
@@ -38,76 +38,75 @@ class _MyBottomSheetState extends State<MyBottomSheet> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              RichText(
-                text: TextSpan(
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
+    return SingleChildScrollView(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Add ',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      TextSpan(
+                        text: 'Sign',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
                   ),
-                  children: [
-                    TextSpan(
-                      text: 'Add ',
-                      style: TextStyle(color: Colors.red),
-                    ),
-                    TextSpan(
-                      text: 'Sign',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
+                ),
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: textEditingController,
+              decoration: InputDecoration(
+                labelText: 'Enter name of your sign',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                String enteredText = textEditingController.text;
+                widget.onTextCreated(enteredText);
+                textEditingController.clear();
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
-              IconButton(
-                icon: Icon(Icons.close),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-          SizedBox(height: 16.0),
-          TextField(
-            controller: textEditingController,
-            decoration: InputDecoration(
-              labelText: 'Enter name of your sign',
-            ),
-          ),
-          SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: () async {
-              String enteredText = textEditingController.text;
-              print(enteredText);
-              await _sendHttpRequest(enteredText);
-              widget.onTextCreated(
-                  enteredText); // Callback to pass text to HomeScreen
-              textEditingController.clear(); // Clear the text field
-              Navigator.pop(context);
-            },
-            style: ElevatedButton.styleFrom(
-              primary: Colors.red,
-              shape: RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.circular(10.0),
+              child: Text(
+                'Create',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
               ),
             ),
-            child: Text(
-              'Create',
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.black,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
-  }
-}
